dd68306e2c4eac25bcedab03e4fc5a300953fa6d
jlantigua133-arch <jlantigua133@gmail.com>
2026-01-07 04:55:14 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Importamos directamente tu página de Checkout
+import Checkout from './Checkout.jsx'  // Si lo guardaste como Checkout.jsx en src/
+
+// Si tienes estilos globales (normalmente sí)
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Muestra el checkout en la página principal */}
+        <Route path="/" element={<Checkout />} />
+        <Route path="/checkout" element={<Checkout />} />
+        {/* Puedes agregar más rutas después */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
